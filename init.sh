a766c06fbb5243ad9312575df2e06fa09b61b5b3
Vineet Kumar Gupta <Vineet.gupta@squadstack.com>
2024-01-12 10:56:13 +0530

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+config_dir="/home/ubuntu/cloudquery"
+Environmentfile="/etc/default/cloudquery"
+
+# Create the directory if it doesn't exist
+if [ ! -d "$config_dir" ]; then
+    mkdir -p "$config_dir"
+fi
+
+dbsecrets=$(aws secretsmanager get-secret-value --secret-id  rds/mux/cloudquery --query SecretString | jq -r)
+db_user=$(echo $dbsecrets | jq '.username' -r)
+db_passwd=$(echo $dbsecrets | jq '.password' -r)
+db_host=$(echo $dbsecrets | jq '.host' -r)
+db_dbname=$(echo $dbsecrets | jq '.dbname' -r)
+
+# Create or update the environment file
+cat > "$Environmentfile" <<EOF
+PG_CONNECTION_STRING = "postgresql://${db_user}:${db_passwd}@${db_host}/${db_dbname}?sslmode=disable"
+EOF
+
+# Set proper permissions for the environment file
+chmod 600 "$Environmentfile"
+
+#  do the symlink 
+sudo ln -svf "${config_dir}/cloudquery.service" "/etc/systemd/system/cloudquery.service"
+sudo ln -svf "${config_dir}/cloudquery.timer" "/etc/systemd/system/cloudquery.timer"
+
+#start the service and timer
+sudo systemctl enable cloudquery.timer
+sudo systemctl start cloudquery.timer
+
+sudo systemctl enable cloudquery.service
+sudo systemctl start cloudquery.service
